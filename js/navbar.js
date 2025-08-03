9a533c939876f25a5462a829cf9f4b031853cbb2
Aman Kumar <amankumarmaurya93@gmail.com>
2025-08-03 22:06:11 +0530

    updating files

--- js/navbar.js
+++ js/navbar.js
@@ -1,24 +1,3 @@
-//  const mobileMenu = document.getElementById('mobile-menu');
-//         const navLinks = document.getElementById('nav-links');
-
-//         mobileMenu.addEventListener('click', () => {
-//             mobileMenu.classList.toggle('active');
-//             navLinks.classList.toggle('active');
-            
-//             // Prevent scrolling when menu is open
-//             document.body.style.overflow = navLinks.classList.contains('active') ? 'hidden' : '';
-//         });
-
-//         // Close menu when a link is clicked (for mobile)
-//         document.querySelectorAll('.nav-links a').forEach(link => {
-//             link.addEventListener('click', () => {
-//                 mobileMenu.classList.remove('active');
-//                 navLinks.classList.remove('active');
-//                 document.body.style.overflow = '';
-//             });
-//         });
-
-
 includeHTML(() => {
     // ✅ All includes done, now run navbar code
     const mobileMenu = document.getElementById("mobile-menu");
